1f5ae1201bfab19a49c7d370d0144bbeaf520a80
Nilson Junior <devs@loocal.online>
2023-10-08 00:59:35 -0300

    fixes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 
 class Product extends Model
 {
@@ -12,6 +14,13 @@ class Product extends Model
     protected $fillable = ['name', 'description', 'category_id', 'price', 'restaurant_id', 'image'];
 
     protected $table = 'products';
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => URL::to('/') . '/storage' . '/' . ($value)
+        );
+    }
 
     public function images() {
         return $this->hasMany(ProductImage::class);
